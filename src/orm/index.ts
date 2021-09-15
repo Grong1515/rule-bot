70dfa009e9351ca9d51f3b145fa567fefd6626eb
Viktor Gronskiy <v.gronskiy@aliradar.com>
2021-09-15 14:04:15 +0200

    added runMigrations on startup

--- src/orm/index.ts
+++ src/orm/index.ts
@@ -11,10 +11,21 @@ class OrmNotInitialized extends Error {
 
 export async function initOrm() {
   if (!orm) orm = await MikroORM.init();
+  if (process.env.RUN_MIGRATIONS === "true") await runMigrations();
   console.log('DB initialized successfully.');
 }
 
 export function getOrm() {
   if (!orm) throw new OrmNotInitialized();
   return orm;
+}
+
+async function runMigrations() {
+  console.log('Run Migrations');
+  if (!orm) throw new OrmNotInitialized();
+  const migrator = orm.getMigrator();
+  const migratinosAmount = (await migrator.getPendingMigrations()).length;
+  console.log(`Found ${migratinosAmount} migrations`);
+  await migrator.up();
+  console.log('Run Migrations: finished successfully');
 }
